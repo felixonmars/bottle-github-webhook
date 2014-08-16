26f0fb82777f2be4b8d2cf25db3e13fb66f6379f
Felix Yan <felixonmars@gmail.com>
2014-08-16 12:11:34 +0800

    Initial port to bottle

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-Flask webhook for Github
+Bottle webhook for Github
 ########################
 A very simple github post-receive web hook handler that executes per default a pull uppon receiving. The executed action is configurable per repository.
 
@@ -17,10 +17,10 @@ Edit ``repos.json`` to configure repositories, each repository must be registere
             "action": [["git", "pull", "origin", "master"], ],
         },
         "d3non/somerandomexample/branch:live": {
-	    "path": "/home/exampleapp",
-	    "action": [["git", "pull", "origin", "live"],
-		["echo", "execute", "some", "commands", "..."] ]
-	}
+            "path": "/home/exampleapp",
+            "action": [["git", "pull", "origin", "live"],
+                ["echo", "execute", "some", "commands", "..."] ]
+        }
     }
 
 Install dependencies.
@@ -34,13 +34,6 @@ Start the server.
 .. code-block:: console
 
     python index.py 80
-
-Start the server behind a proxy (see: http://flask.pocoo.org/docs/deploying/wsgi-standalone/#proxy-setups)
-
-.. code-block:: console
-
-    USE_PROXYFIX=true python index.py 8080
-
 
 
 Go to your repository's settings on `github.com <http://github.com>`_ and register your public URL under ``Service Hooks -> WebHook URLs``.
--- index.py
+++ index.py
@@ -7,12 +7,11 @@ import json
 import subprocess
 import requests
 import ipaddress
-from flask import Flask, request, abort
+import logging
+from bottle import route, run, request, abort
 
-app = Flask(__name__)
 
-
-@app.route("/", methods=['GET', 'POST'])
+@route("/", method=['GET', 'POST'])
 def index():
     # Store the IP address blocks that github uses for hook requests.
     hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
@@ -25,45 +24,46 @@ def index():
         for block in hook_blocks:
             ip = ipaddress.ip_address(u'%s' % request.remote_addr)
             if ipaddress.ip_address(ip) in ipaddress.ip_network(block):
-                break #the remote_addr is within the network range of github
+                break  # the remote_addr is within the network range of github
         else:
             abort(403)
 
         if request.headers.get('X-GitHub-Event') == "ping":
             return json.dumps({'msg': 'Hi!'})
         if request.headers.get('X-GitHub-Event') != "push":
-	    return json.dumps({'msg': "wrong event type"})
+            return json.dumps({'msg': "wrong event type"})
 
         repos = json.loads(io.open('repos.json', 'r').read())
 
-        payload = json.loads(request.data)
+        payload = request.json
         repo_meta = {
-	    'name': payload['repository']['name'],
-	    'owner': payload['repository']['owner']['name'],
-	    }
-	match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
-	if match:
-	    repo_meta['branch'] = match.groupdict()['branch']
-	    repo = repos.get('{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
+            'name': payload['repository']['name'],
+            'owner': payload['repository']['owner']['name'],
+        }
+        match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
+        if match:
+            repo_meta['branch'] = match.groupdict()['branch']
+            repo = repos.get('{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
         else:
-	    repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
+            repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
         if repo and repo.get('path', None):
-	    if repo.get('action', None):
-	        for action in repo['action']:
-		    subprocess.Popen(action,
-                             cwd=repo['path'])
-	    else:
-		subprocess.Popen(["git", "pull", "origin", "master"],
-                             cwd=repo['path'])
+            if repo.get('action', None):
+                for action in repo['action']:
+                    subprocess.Popen(action,
+                                     cwd=repo['path'])
+            else:
+                subprocess.Popen(["git", "pull", "origin", "master"],
+                                 cwd=repo['path'])
         return 'OK'
 
 if __name__ == "__main__":
-    try:
-        port_number = int(sys.argv[1])
-    except:
+    if len(sys.argv) > 1:
+        try:
+            port_number = int(sys.argv[1])
+        except:
+            logging.error("Invalid port_number specified")
+            sys.exit(1)
+    else:
         port_number = 80
     is_dev = os.environ.get('ENV', None) == 'dev'
-    if os.environ.get('USE_PROXYFIX', None) == 'true':
-	from werkzeug.contrib.fixers import ProxyFix
-	app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.run(host='0.0.0.0', port=port_number, debug=is_dev)
+    run(host='0.0.0.0', port=port_number, debug=is_dev)
